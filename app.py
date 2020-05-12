071cf26315fc60f32ca9aa76c72b209b6753453c
Kevin Dryfuse <kevin@kevindryfuse.com>
2020-05-11 21:38:17 -0400

    adding templates for heroku deploy

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, make_response
 from flask_web.domains.Players import Players
+
 
 from flask_web.domains.Game import Game
 
@@ -9,15 +10,17 @@ app = Flask(__name__)
 # TODO: Add endpoints to setup a game instead of the hardcoded values
 # TODO: Add some visuals and track statistics
 # TODO: All the things
-@app.route('/CandyLand/<players>')
+@app.route('/CandyLand/<players>', methods=["GET"])
 def single_game(players):
     players = Players(players)
     game = Game(players)
     game.play_game()
-    return "Player " + game.winner.get_name() + " won the game!"
+    print(game.winner.get_name())
+    # return "Player " + game.winner.get_name() + " won the game!"
+    return render_template('template.html', my_string=game.winner.get_name(), my_list=[0,1,2,3,4,5])
 
 
-@app.route('/CandyLand/<players>/games/<games>')
+@app.route('/CandyLand/<players>/games/<games>', methods=["GET"])
 def more_than_one_game(players, games):
     game_players = Players(players)
 
@@ -31,9 +34,13 @@ def more_than_one_game(players, games):
     for player in game.get_players():
         return_string += " - Player " + player.get_name() + " won the game " + str(player.victory_count) + " times!\n"
 
-    return return_string
+    response = make_response(return_string, 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
+
 
 if __name__ == '__main__':
+
     app.run(debug=True,
             host='0.0.0.0')
 
